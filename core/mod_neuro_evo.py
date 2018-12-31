b26815fae0d8783d2f6e53c423c819badff20702
zhan0903 <z.han0903@qq.com>
2018-12-31 11:38:37 +1100

    debug

--- core/mod_neuro_evo.py
+++ core/mod_neuro_evo.py
@@ -6,6 +6,9 @@ import logging
 
 logger = logging.getLogger(__name__)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 logger.setLevel(level=logging.DEBUG)
 
 
