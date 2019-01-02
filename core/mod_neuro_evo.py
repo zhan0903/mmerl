5ea05b14e4493c744a123cdbe61b6ec1b6a74437
zhan0903 <z.han0903@qq.com>
2019-01-03 09:23:49 +1100

    debug

--- core/mod_neuro_evo.py
+++ core/mod_neuro_evo.py
@@ -217,10 +217,10 @@ class SSNE:
         for i in range(self.population_size):
             if i not in new_elitists:  # Spare the new elitists
                 assert self.args.mutation_prob == 0.9
-                logger.debug("pop[i]:{}".format(pop[i].state_dict()))
+                logger.debug("pop[i]:{}".format(pop[i].state_dict()["w_out.bias"]))
                 # if random.random() < self.args.mutation_prob:
                 self.mutate_inplace(pop[i], num_frames)
-                logger.debug("pop[i]:{}".format(pop[i].state_dict()))
+                logger.debug("pop[i]:{}".format(pop[i].state_dict()["w_out.bias"]))
 
         return new_elitists[0]
 
